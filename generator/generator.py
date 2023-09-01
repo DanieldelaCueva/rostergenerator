bc861d3dbaffe8770b23893d71450659ba5c26bc
DanieldelaCueva <76701720+DanieldelaCueva@users.noreply.github.com>
2023-09-01 12:23:29 +0200

    created generator function

---
+++ generator/generator.py
@@ -0,0 +1,33 @@
+import jinja2
+import pdfkit
+
+def generate_pdf(context, player_list, coach_list):
+    template_loader = jinja2.FileSystemLoader('./')
+    template_env = jinja2.Environment(loader=template_loader)
+
+    html_template = "/templates/roster_template.html"
+    template = template_env.get_template(html_template)
+
+    output_text = template.render(context)
+
+    insert1_index = output_text.find("<!--insert1-->")+14
+
+    add1 = ""
+
+    for player in player_list:
+        add1 += f"<tr style='height: 18px;'><td style='width: 10%; height: 18px; text-align: center;'>{player['num']}</td><td style='width: 45%; height: 18px;'>{player['surname']}</td><td style='width: 45%; height: 18px;'>{player['name']}</td></tr>"
+
+    prov1_text = output_text[:insert1_index] + add1 + output_text[insert1_index:]
+
+    insert2_index = prov1_text.find("<!--insert2-->")+14
+
+    add2 = ""
+
+    for coach in coach_list:
+        add2 += f"<tr style='height: 18px;'><td style='width: 10%; height: 18px; text-align: center;'>{coach['num']}</td><td style='width: 45%; height: 18px;'>{coach['surname']}</td><td style='width: 45%; height: 18px;'>{coach['name']}</td></tr>"
+
+    prov2_text = prov1_text[:insert2_index] + add2 + prov1_text[insert2_index:]
+
+    config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+    output_pdf = "output.pdf"
+    pdfkit.from_string(prov2_text, output_pdf, configuration=config)
---
+++ main.py
@@ -0,0 +1,4 @@
+from env.env import *
+from generator.generator import generate_pdf
+
+generate_pdf(context, player_list, coach_list)
